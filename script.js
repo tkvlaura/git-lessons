5ecf5aa7ed3ef5af87acedc17f8d21e9e00cf6f9
Laura <laura.tokayeva@gmail.com>
2024-01-21 00:24:07 +0300

    commit2

--- script.js
+++ script.js
@@ -2,9 +2,22 @@
 
 let number = 100;
 number = 200;
-let massage = "bye-bye";
-massage = "bye"
+let message = "bye-bye";
+message = "bye";
 let isTall = true ;
 let vegetables = [ "tomato" , "potato"]
 
+var name = prompt("Имя студента")
+var adress = prompt("Адрес")
+var phone = prompt("Номер телефона")
+
+
+
 console.log (number)
+console.log (message)
+console.log (isTall)
+console.log (vegetables)
+
+
+
+
