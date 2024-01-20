8c624982d867a074482795041ba50dadf3e949b4
Laura <laura.tokayeva@gmail.com>
2024-01-21 01:55:03 +0300

    commit678

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+// 3 задание
+
+let number = 100;
+number = 200;
+let message = "bye-bye";
+message = "bye";
+let isTall = true ;
+let vegetables = [ "tomato" , "potato"]
+
+var name = prompt("Имя студента")
+var adress = prompt("Адрес")
+var phone = prompt("Номер телефона")
+
+
+
+console.log (number)
+console.log (message)
+console.log (isTall)
+console.log (vegetables)
+
+
+
+
+
