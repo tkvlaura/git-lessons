4fa9f479b8f109408338eada563ef3d9bd7c5766
Laura <laura.tokayeva@gmail.com>
2024-02-19 14:17:05 +0300

    coomit 1

--- script.js
+++ script.js
@@ -1,26 +1,157 @@
+// 1 задание
+// function sum2 (x,c)
+//  {return console.log (x + c)}
+// sum2(20, 20)
 
+// 2 задание
+// function sum1 (s,d)
+// {return console.log (s * d)}
+// sum1(5, 10)
 
 // 3 задание
+// let h = 5;
+// if(h > 0 )
+// { console.log("переменная h больше 0")
+// } else { console.log("переменная h меньше 0")}
+  
 
-let number = 100;
-number = 200;
-let message = "bye-bye";
-message = "bye";
-let isTall = true ;
-let vegetables = [ "tomato" , "potato"]
+// 4 задание
+// let a = 20;
+// let b = 5;
+// let z = a + b;
+// let q = a * b;
+// let w = a / b;
+// let p = a - b;
+// console.log(z)
+// console.log(q)
+// console.log(w)
+// console.log(p)
 
-var name = prompt("Имя студента")
-var adress = prompt("Адрес")
-var phone = prompt("Номер телефона")
+// 5 задание
+// let xx = true;
+// let yy = false;
+// let cc = xx < yy;
+// let ee = xx > yy;
+// let rr = xx == yy;
+// let tt = xx != yy;
+// console.log(cc)
+// console.log(ee)
+// console.log(rr)
+// console.log(tt)
+
+  // 6 задание
+  //let age = prompt('Возраст?', 18);
+ // let message = (age < 18) ? 'Подросток' :
+ //   (age < 18) ? 'Взрослый' :
+ //   (age < 60) ? 'Здравствуйте!' :
+ //   'Пожилой';
+//  alert( message );
+
+
+  // 7 задание
+ // function printSquares (numb1, numb2)  {
+ // console.log("Квадрат числа " + numb1 + " равен " + (numb1 * numb1));
+ // console.log("Квадрат числа " + numb2 + " равен " + (numb2 * numb2));
+ //}
+ // printSquares(2,4)
+ 
+
+ // 8 задание 
+ // let counter = 0;
+ // counter += 5;
+ // console.log(counter);
+
+
+ // 9 задание
+ // let text = "привет"; 
+ // console.log(text == "привет"); 
+ // console.log(text != "привет"); 
+ 
+
+ // 10 задание
+ /* function sum(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+  sum += array[i];
+  }
+  return sum;
+  }
+
+  let numbers = [1, 2, 3, 4, 5]
+  console.log(sum(numbers));
 
 
 
-console.log (number)
-console.log (message)
-console.log (isTall)
-console.log (vegetables)
+ // 11 задание
+// const age = 20;
+ //let isAdult = true;
+ //age > 18 ? isAdult : false;
+//console.log(age > 18? isAdult : false)
+
+
+// 11 задание
+//const age = 20;
+//let isAdult = true;
+//let res = age > 18 ? isAdult : false;
+//console.log(res);
 
 
 
 
 
+ // 1 задание
+ // let age = prompt("Сколько вам лет?")
+ // if (age >= 18) { console.log("Вы можете проходить тестирование");
+ // }
+ // else{ console.log ("Вам запрещено проходить тестирование");}
+
+ // 2 задание
+// let poiu = prompt("Введите строку");
+// if (poiu === "") {console.log ("Пустая строка");}
+// else {console.log ("Заполненная строка");}
+
+
+
+
+
+
+
+
+  // Функции и их использование
+  // 1 задание
+// function sum3 (x,c)
+// {return x + c}
+//sum3(20, 20)
+//const result = sum3(20, 20);
+//console.log(sum3);
+
+// 2 задание
+// let string1 = "Приветик"
+// let len = string1.length;
+// console.log(len);
+
+// 3 задание
+// function calc(num1, num2, operator) {
+//  if (operator === '+') {
+//      return num1 + num2;
+//  } else if (operator === '-') {
+//      return num1 - num2;
+//  } else if (operator === "/") {
+//    return num1 / num2;
+//  }
+// else if (operator === "/") {
+//    return num1 / 0;     }
+ // }
+// calc(5, 5, "+");
+// console.log(calc(5, 5, "+"));
+// console.log(calc(5, 5, "-"));
+// console.log(calc(5, 5, "/"));
+// console.log(calc(5, 0, "/"));
+
+// 4 задание
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const result = array.filter((number) => {
+//    return number > 6
+//})
+// console.log(result);
